69559736a3b4b8be768ea4584b522f0cb6ba75b3
VDC2302 <otamegane159@gmail.com>
2023-05-05 23:57:28 +0700

    update AccountRouter

--- routers/AccountRouter.js
+++ routers/AccountRouter.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const Router = express.Router()
 const {validationResult} = require('express-validator')
-const AccountController = require('../controller/AccountController')
+const AccountController = require('../controllers/AccountController')
 
 const registerValidator = require('../routers/validators/registerValidator')
 const loginValidator = require('../routers/validators/loginValidator')
 
 
-Router.post('/login', AccountController.loginValidator)
+Router.post('/login', loginValidator, AccountController.loginValidator)
 
-Router.post('/register', AccountController.registerValidator)
+Router.post('/register', loginValidator, AccountController.registerValidator)
 
 module.exports = Router
