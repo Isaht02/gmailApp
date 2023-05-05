d78355117da040d17e991598767598a7bb717f52
VDC2302 <otamegane159@gmail.com>
2023-05-05 22:34:46 +0700

    Update AccountModel.js

--- models/AccountModel.js
+++ models/AccountModel.js
@@ -4,10 +4,26 @@ const Schema = mongoose.Schema
 const AccountSchema = new Schema({
     email: {
         type:String,
+        required: true,
         unique: true
     },
-    password: String,
-    fullname: String
+    password: { 
+        type: String,
+        required: true
+    },
+    fullname: {
+        type: String,
+        require: true
+    },
+    avatar: String,
+
+    mailbox: {
+        inbox: [{type: mongoose.Schema.Types.ObjectId, ref: 'Email'}],
+        outbox: [{type: mongoose.Schema.Types.ObjectId, ref: 'Email'}],
+        draft: [{type: mongoose.Schema.Types.ObjectId, ref: 'Email'}],
+        trash: [{type: mongoose.Schema.Types.ObjectId, ref: 'Email'}]
+    }
+
 })
 
 module.exports = mongoose.model('Account', AccountSchema)
